480ea69d4e4d16f148b7ea1c19c530f833042d4f
Arzhevitin Bogdan Kirillovich <arzhevitin.bk@gmail.com>
2018-01-24 01:34:29 +0300

    0.2v  - Рабочая версия. Но необходимо провести дополнительные тесты

--- Source1.c
+++ Source1.c
@@ -1,19 +1,20 @@
 #include "stdio.h"
 
 //extern void print(char *out_buf, const char *format, const char *hex_number);
-extern void test_asm(char *out_buf, const char *hex_number);
+extern void test_asm(char *out_buf, const char *format, const char *hex_number);
 
 int main()
 {
   char *out_buf = malloc(sizeof(char) * 256);
-  char *hex_number = malloc(sizeof(char) * 33);
+  char *format = malloc(sizeof(char) * 256);
+  char *hex_number = malloc(sizeof(char) * 34);
+                                 
+  snprintf(format, 256, "%s\0", "0-10");   //"<-+ 012><wight>"
+  snprintf(hex_number, 256, "%s\0", "-ffffffffffffffffffffffffffffff21"); //<->
 
-  snprintf(hex_number, 256, "%s\0", "fffffffffffffffffffffffffffffff1");
-  //snprintf(out_buf, 1024, "%s\0", "12345678901234567890123456789012");
+  test_asm(out_buf, format, hex_number);
 
-  test_asm(out_buf, hex_number);
-
-  printf("out_buf: %s\nhex_number: %s\n", out_buf, hex_number);
+  printf("out_buf:%s.\nhex_number:%s\n", out_buf, hex_number);
 
   free(out_buf);
   free(hex_number);
--- Source2.asm
+++ Source2.asm
@@ -8,6 +8,8 @@ section .data
   val_count:    dd 0     
   array_val:    times 4 dd 0
   is_neg_sign:  db 0
+  flags_arr:        times 4 db 0    ; [flags_arr]==1 > '-' [flags_arr+1]==1 > '+' [flags_arr+2]==1 > ' ' [flags_arr+3]==1 > '0'
+  wight:        dd 0            ; Ширина поля форматирования   
 
 section .text
   global _test_asm
@@ -17,7 +19,7 @@ section .text
     push  esp     ; итого 40
 
     ; --- Посчитаем кол-во символов и запишем в [val_count] --- 
-    mov   eax, dword [esp+40+4*2]  ; Помещаем указатель на строку в EAX
+    mov   eax, dword [esp+40+4*3]  ; Помещаем указатель на строку в EAX
     xor ecx, ecx
     _string_count:
       mov   bl, byte [eax+ecx]         ; Помещаем в символ в BL
@@ -31,7 +33,7 @@ section .text
 
     ; --- Начнем преобразование  --- 
     ; Проверка на символ инвертирования перед числом '-'
-    mov   eax, dword [esp+40+4*2]  ; Помещаем указатель на строку в EAX
+    mov   eax, dword [esp+40+4*3]  ; Помещаем указатель на строку в EAX
     mov   bl, byte [eax]         ; Помещаем в символ в BL
     cmp   bl, '-'                     ; Перед числом стоит '-', значит нужно потом инвертировать число и уменьшить счетчик символов
     jne _endif_invert
@@ -46,7 +48,7 @@ section .text
     ; Все ок, приступаем к обработке числа
     xor   ecx, ecx
     _string_to_array_val:
-      mov   eax, dword [esp+40+4*2]         ; Помещаем указатель на строку в EAX
+      mov   eax, dword [esp+40+4*3]         ; Помещаем указатель на строку в EAX
       add   eax, [is_neg_sign]              ; Если был символ '-', сдвигаем еще на один байт
       mov   bl, byte [eax+ecx]              ; Помещаем в символ в BL     
 
@@ -124,10 +126,6 @@ section .text
       xor esi, esi              ; В esi запишем кол-во dex цифр
 
       ; ------ Инверсия если нужно 
-      nop
-      nop
-      nop
-
       mov eax, 1
       cmp   [is_neg_sign], eax
       jne   _no_invese_sign       ; Если нет знака '-' инвертирования числа, идем дальше
@@ -185,32 +183,254 @@ section .text
       jnz _convert_to_literal_and_add_to_stack
 
       mov [val_count], esi              ; Сбросим значение счетчика hex цифр, будем использовать его для кол-ва dex чисел   
-      ; ---------------------------------------------------
+    ; ---------------------------------------------------
 
 
-      ; ------ Запись в [out_buffer] строки из стека
-      mov   eax, [esp+40+4+esi*4]             ; в eax указатель на out_buffer (esi*4 - т.к. в стеке уже есть цифры)
+    ; ------ Ну вот и добрались мы до флагов форматирования, кажется это самая отстойная часть кода моей программы 
 
-      xor ecx, ecx                      ; Счетчик для цикла _loop_write_to_out_buf
+nop
+nop
+nop
 
-      mov edx, 1             ; Проверка, нужно ли писать символ минуса перед числом?
-      cmp [is_neg_sign], edx
-      jne _loop_write_to_out_buf
-        mov bl, '-'
-        mov [eax], bl               ; Запись запись минуса перед числом
-        inc ecx                         ; Увеличим счетчик символов на один
-        add [val_count], edx       ; Добавляем еще один символ
+    add   eax, [val_count]         ; Помещаем указатель на строку формата в EAX
+    mov   ebx, 4                    ; с условием сдвига стека из-за добавленых в него символов
+    mul   ebx
+    lea   ebx, dword [esp+40+4*2]  
+    add   eax, ebx
+    mov   eax, [eax]
 
-      _loop_write_to_out_buf:
-        pop ebx                           ; в ebx находится десятичное число
-        add ebx, 48                       ; преобразуем в ascii код 
+    mov   dl, 1                ; Для установки значения флагов
+    xor   ecx, ecx              ; Храним индекс обрабатываемого символа строки формата
+    _string_flag_next_symbol:   ; Посимвольно считываем флаги из строки, пока не встретим цифру [1-9] либо '\0'
+      mov   bl, byte [eax+ecx]         ; Помещаем в символ в BL
+      inc   ecx 
+
+      cmp   bl, 0                   ; Если BL==0, значит строка полностью обработана
+      je   _string_flag_without_wight
+        
+      cmp   bl, '-'
+      jne   _not_flag_0
+        mov byte [flags_arr], dl
+    jmp _string_flag_next_symbol
+      _not_flag_0:
+      
+      cmp   bl, '+'
+      jne   _not_flag_1
+        mov byte [flags_arr+1], dl
+    jmp _string_flag_next_symbol
+      _not_flag_1:
+
+      cmp   bl, ' '
+      jne   _not_flag_2
+        mov byte [flags_arr+2], dl
+    jmp _string_flag_next_symbol
+      _not_flag_2:
+
+      cmp   bl, '0'
+      jne   _not_flag_3
+        mov byte [flags_arr+3], dl
+    jmp _string_flag_next_symbol
+      _not_flag_3:
+        
+    ; Если очередной символ не один из символов флагов, значит это первая цифра поля ширины (wight)
+
+    ; Подсчитаем кол-во символов поля wight, в edx будем хранить это кол-во символов 
+
+    xor edx, edx
+    dec ecx                         ; Сдвинем на верную позицию
+    _string_wight_count_symbol:   
+      inc   edx                      ; Так как сейчас eax+ecx уже указывает на первый символ поля (wight), учтем это   
+      mov   bl, byte [eax+ecx]  
+      inc   ecx
+      cmp   bl, 0                   ; Если BL==0, значит строка полностью обработана
+    jne   _string_wight_count_symbol
+
+    dec   edx
+    dec   ecx                          ; Теперь eax+ecx будет указывать на последний символ поля (wight)
+    add   eax, ecx                     ; Теперь eax будет указывать на последний символ поля (wight)
+    dec   eax
+
+    ; Преобразуем поле (wight) к DEC и запишем в переменную [wight]
+    xor   esi, esi                    ; В esi храним тещую итерацию цикла для правильного умножения  
+    xor   ebx, ebx
+    mov   edi, eax                    ; В edi временно будем хранить текущий адрес символа поля (wight)
+
+    _string_wight_symbol_to_val:   
+      push  edx                     ; Запомним кол-во смволов оставшегося поля (wight)
+      mov   bl, byte [edi]  
+      sub   bl, 48                  ; Преобразуем bl к dec числу
+      
+      mov   eax, ebx               ; Переместим в eax значение ebx для последующего умножения на 10 в степени 
+      mov   ecx, esi               ; Счетчик цикла возведения 10 в степень esi
+      _loop_mul_10_start:
+        cmp   ecx, 0
+        je    _loop_mul_10_end
+        dec   ecx
+        mov   edx, 10
+        mul   edx   
+        jmp _loop_mul_10_start            ; Теперь eax правильно умноженная цифра числ
+      _loop_mul_10_end:
+
+      add   [wight], eax              ; Добавим очередную цифру
+      
+      inc   esi
+      dec   edi
+      pop   edx                     ; Восстановим значение кол-ва смволов поля (wight)
+      dec   edx
+    jnz   _string_wight_symbol_to_val
+
+    _string_flag_without_wight:
+    ; Обработка завершена, в [widgt] размер поля, во [flags_arr..] значения требуемых флагов
+    
+
+    ; ------ Запись в [out_buffer] строки из стека
+    mov   esi, [val_count]
+    mov   eax, [esp+40+4+esi*4]             ; в eax указатель на out_buffer (esi*4 - т.к. в стеке уже есть цифры)
+
+    xor ecx, ecx                      ; Счетчик текущего индекса [eax+ecx] для записи в строку 
+
+    ; Добавление символа перед числом 
+
+    mov dl, 1             
+    cmp byte [flags_arr+2], dl
+    jne _skip_flag_2
+    mov bl, ' '
+    _skip_flag_2: 
+    cmp byte [flags_arr+1], dl
+    jne _skip_flag_1
+    mov bl, '+'
+    _skip_flag_1:
+     
+    cmp byte [is_neg_sign], dl
+    jne _skip_neg_sign
+    mov bl, '-'
+    _skip_neg_sign:
+
+    xor dh, dh                      ; Проверка на то, были ли установлены флаги установки символа перед числом
+    add dh, byte [flags_arr+1]
+    add dh, byte [flags_arr+2]
+    add dh, byte [is_neg_sign]
+    cmp dh, 0                      ;Если значение не 0, то были установлены флаги установки символа перед числом
+    jz _skip_inc_ecx
+      mov [eax], bl               ; Запись символа перед числом
+      inc ecx                         ; Увеличим счетчик символов на один
+      add [val_count], dl       ; Добавляем еще один символ
+    _skip_inc_ecx:
+   
+    ; ---- Добавим в строку символ заполнитель 
+    
+     ;;;; Установлен flag_0 -> '-' ?    
+    cmp byte [flags_arr], dl      
+    jne _no_flag_2
+      ; Запишем число со знаком
+      _loop_write_to_out_buf_1:   
+        pop ebx                          ; в ebx находится десятичное число
+        add ebx, 48                      ; преобразуем в ascii код 
         mov [eax+ecx], bl               ; Запись очередного символа с необходимым сдвигом out_buff
         inc ecx
         cmp ecx, [val_count]
-      jl _loop_write_to_out_buf
+      jl _loop_write_to_out_buf_1
+      ; Запишем заполнение
+      _loop_write_to_out_placeholder_1:   
+        cmp ecx, [wight]
+        jge _end_string_parse
+        mov bl, ' '                      
+        mov [eax+ecx], bl               
+        inc ecx      
+      jmp _loop_write_to_out_placeholder_1
+    _no_flag_2: 
 
-      mov   bl, 0                         ; Закончим строку символом \0
-      mov   [eax+ecx], bl
+    ;;;; Установлен flag_3 -> '0' ?
+    cmp byte [flags_arr+3], dl    
+    jne _no_flag_3
+      mov esi, [wight]            ; Проверим ширину поля 
+      cmp esi, [val_count]
+      jg _ok_2
+        mov esi, [val_count]
+        cmp dh, 0                      ; Проверка на то, есть ли любой спец знак перед числом
+        jz _no_eny_sign12
+          ;dec esi
+        _no_eny_sign12:
+        jmp _loop_write_to_out_buf_2 ; Установленная ширина поля мала
+      _ok_2:  
+      sub esi, [val_count]          ; Теперь в esi находится кол-во требующихся символов заполнителей '0' для печати перед числом 
+
+      ; Запишем заполнение
+      _loop_write_to_out_placeholder_2:   
+        mov bl, '0'                      
+        mov [eax+ecx], bl               
+        inc ecx
+        cmp ecx, esi
+      jl _loop_write_to_out_placeholder_2
+
+      add esi, [val_count]              ; Запишем в esi общую длину символов числа для вывода
+      ; Проверка на то, есть ли любой спец знак перед числом 
+      cmp dh, 0                      ;Если значение не 0, то были установлены флаги установки символа перед числом
+      jz _no_eny_sign
+        ;dec esi
+      _no_eny_sign:
+
+      ; Запишем число со знаком
+      _loop_write_to_out_buf_2:   
+        pop ebx                          ; в ebx находится десятичное число
+        add ebx, 48                      ; преобразуем в ascii код 
+        mov [eax+ecx], bl               ; Запись очередного символа с необходимым сдвигом out_buff
+        inc ecx
+        cmp ecx, esi
+      jl _loop_write_to_out_buf_2
+      jmp _end_string_parse
+    _no_flag_3: 
+
+
+    ;;;; Если   flag_0 и  flag_3 не установлены
+    mov esi, [wight]            ; Проверим ширину поля 
+    cmp esi, [val_count]
+    jg _ok_3
+      mov esi, [val_count]
+      cmp dh, 0                      ; Проверка на то, есть ли любой спец знак перед числом
+      jz _no_eny_sign13
+        ;dec esi
+      _no_eny_sign13:
+      jmp _loop_write_to_out_buf_3 ; Установленная ширина поля мала
+    _ok_3:
+    sub esi, [val_count]          ; Теперь в esi находится кол-во требующихся символов заполнителей '0' для печати перед числом 
+  
+    cmp dh, 0                      ; Проверка на то, есть ли любой спец знак перед числом
+    jz _no_eny_sign1
+      ;inc esi
+      mov bh, [eax]   ; теперь в bh лежит знак
+      dec ecx
+    _no_eny_sign1:
+    
+    ; Запишем заполнение
+    _loop_write_to_out_placeholder_3:   
+      mov bl, ' '                      
+      mov [eax+ecx], bl               
+      inc ecx
+      cmp ecx, esi
+    jl _loop_write_to_out_placeholder_3
+    
+    add esi, [val_count]
+    cmp dh, 0                      ; Запись спец знака перед числом
+    jz _no_eny_sign2
+      mov [eax+ecx], bh               
+      inc ecx
+      ;dec esi
+    _no_eny_sign2:
+
+    ; Запишем число со знаком
+    _loop_write_to_out_buf_3:    
+      pop ebx                           ; в ebx находится десятичное число
+      add ebx, 48                       ; преобразуем в ascii код 
+      mov [eax+ecx], bl               ; Запись очередного символа с необходимым сдвигом out_buff
+      inc ecx
+      cmp ecx, esi
+    jl _loop_write_to_out_buf_3
+
+    _end_string_parse:
+
+    mov   bl, 0                         ; Закончим строку символом \0
+    mov   [eax+ecx], bl
 
     ; ------------------------------------------------------
 
